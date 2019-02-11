4879f4eb64bb87e4f04196652bee34905e908c0a
IgorKilipenko <igorkili@mail.ru>
2019-02-11 16:23:26 +0700

    sfix

--- m8t-tcp-reciver.ino
+++ m8t-tcp-reciver.ino
@@ -4,14 +4,6 @@
 
 #define BAUND 115200
 
-#ifndef ALTSSID
-#define TEL_SSID "Keenetic-9267"
-#define TEL_PASS "1234567890"
-#else
-#define TEL_SSID "Redmi5Igor123"
-#define TEL_PASS "1234567890"
-#endif
-
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
