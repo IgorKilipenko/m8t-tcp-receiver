cec4f7d8001be22c606c5cf46aef772e98ef7b96
IgorKilipenko <igorkili@mail.ru>
2019-02-20 21:44:46 +0700

    first commit

---
+++ libs/AsyncWebServer.h
@@ -0,0 +1,12 @@
+#ifndef AsyncWebServer_h
+#define AsyncWebServer_h
+
+class AsyncWebServer{
+    public:
+    AsyncWebServer();
+    ~AsyncWebServer();
+    void setup();
+
+};
+
+#endif  // AsyncWebServer_h
