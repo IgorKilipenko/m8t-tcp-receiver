b5270c3c116193902d07a7a3a8e5d86d4646a328
IgorKilipenko <igorkili@mail.ru>
2019-11-23 21:37:49 +0700

    add wifi events
    Signed-off-by: IgorKilipenko <igorkili@mail.ru>

---
+++ lib/WiFiWithEvents/src/WiFiWithEvents.h
@@ -0,0 +1,19 @@
+#ifndef WiFiWithEvents_h
+#define WiFiWithEvents_h
+
+class WiFiWithEvents {
+  public:
+	WiFiWithEvents() {}
+	~WiFiWithEvents() {}
+
+    void connectSta() {
+        WiFi.begin(ssid, password);
+    }
+
+
+  private:
+	const char *ssid = "Keenetic-9267";
+	const char *password = "1234567890";
+};
+
+#endif // WiFiWithEvents_h
